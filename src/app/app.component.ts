53906d8f9aec9f5419e8cb2e51febc417f0f9a1a
vicente1992 <vic_ortiz20@hotmail.es>
2024-11-13 14:14:06 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgxIndexedDBService } from 'ngx-indexed-db';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-ngx-indexed-db';
+
+  people: any[] = [];
+  peoples = signal<any[]>([]);
+  constructor(private dbService: NgxIndexedDBService) {
+    this.dbService.clear('people').subscribe((successDeleted) => {
+      console.log('success? ', successDeleted);
+    });
+
+    this.dbService
+      .bulkAdd('people', [
+        {
+          name: `charles number ${Math.random() * 10}`,
+          email: `email number ${Math.random() * 10}`,
+        },
+        {
+          name: `charles number ${Math.random() * 10}`,
+          email: `email number ${Math.random() * 10}`,
+        },
+      ])
+      .subscribe((result) => {
+        console.log('result: ', result);
+      });
+  }
+
+  ngOnInit() {
+    this.dbService.getAll('people')
+      .pipe(tap((result) => this.peoples.set(result)))
+      .subscribe( );
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,36 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { DBConfig, NgxIndexedDBModule } from 'ngx-indexed-db';
+
+const dbConfig: DBConfig = {
+  name: 'MyDb',
+  version: 1,
+  objectStoresMeta: [
+    {
+      store: 'people',
+      storeConfig: { keyPath: 'id', autoIncrement: true },
+      storeSchema: [
+        { name: 'name', keypath: 'name', options: { unique: false } },
+        { name: 'email', keypath: 'email', options: { unique: false } },
+      ],
+    },
+    {
+      store: 'posts',
+      storeConfig: { keyPath: 'id', autoIncrement: true },
+      storeSchema: [
+        { name: 'title', keypath: 'title', options: { unique: false } },
+        { name: 'body', keypath: 'body', options: { unique: false } },
+      ]
+    },
+  ],
+};
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    importProvidersFrom(NgxIndexedDBModule.forRoot(dbConfig)),
+  ]
 };
